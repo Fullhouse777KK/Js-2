27eb5ccdd968d621e413be4fb645d4c163769116
fullhouse777KK <gafur777_97@mail.ru>
2023-12-12 14:17:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,230 @@
+var age = +prompt('Введите свой возраст')
+
+
+switch(age){
+    case 1:
+    case 2: 
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+alert('Вам нужно учиться')
+break;
+    case 18:
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+    case 25:
+    case 26:
+    case 27:
+    case 28:
+    case 29:
+    case 30:
+    case 31:
+    case 32:
+    case 33:
+    case 34:
+    case 35:
+    case 36:
+    case 37:
+    case 38:
+    case 39:
+    case 40:
+    case 41:
+    case 42:
+    case 43:
+    case 44:
+    case 45:
+    case 46:
+    case 47:
+    case 48:
+    case 49:
+    case 50:
+alert('Вам нужно работать')
+break;
+    case 50:
+    case 51:
+    case 52:
+    case 53:
+    case 54:
+    case 55:
+    case 56:
+    case 57:
+    case 58:
+    case 59:
+alert('Вам скоро на пенсию')
+break;
+        case 60:
+        case 61:
+        case 62:
+        case 63:
+        case 64:
+        case 65:
+        case 66:
+        case 67:
+        case 68:
+        case 69:
+        case 70:
+        case 71:
+        case 72:
+        case 73:
+        case 74:
+        case 75:
+        case 76:
+        case 77:
+        case 78:
+        case 79:
+        case 80:
+        case 81:
+        case 82:
+        case 83:
+        case 84:
+        case 85:
+        case 86:
+        case 87:
+        case 88:
+        case 89:
+        case 90:
+        case 91:
+        case 92:
+        case 93:
+        case 94:
+        case 95:
+        case 96:
+        case 97:
+        case 98:
+        case 99:
+        case 100:
+alert('Вы пенсионер')
+break;
+        
+default:
+alert('Ошибка')
+break;
+}
+
+var Clock = +prompt('Введите время')
+
+switch(Clock){
+    case 1:
+    alert('Час ночи')
+break;
+    case 2:
+    alert('Два часа ночи')
+break;
+    case 3:
+    alert('Три часа ночи')
+break;
+    case 4:
+    alert('Четыре ночи')
+break;
+    case 5:
+    alert('Пять утра')
+break;
+    case 6:
+    alert('Шесть утра')
+break;
+    case 7:
+    alert('Семь утра')
+break;
+    case 8:
+    alert('Восемь утра')
+break;
+    case 9:
+    alert('Девять утра')
+break;
+    case 10:
+    alert('Десять утра')
+break;
+    case 11:
+    alert('Одиннадцать утра')
+break;
+    case 12:
+    alert('Двеннадцать часов полудня')
+break;
+    case 13:
+    alert('Час дня')
+break;
+    case 14:
+    alert('Два часа дня')
+break;
+    case 15:
+    alert('3 часа дня')
+break;
+    case 16:
+    alert('Четыре часа дня')
+break;
+    case 17:
+    alert('Пять часов вечера')
+break;
+    case 18:
+    alert('Шесть часов вечера')
+break;
+    case 19:
+    alert('Семь часов вечера')
+break;
+    case 20:
+    alert('Восемь часов вечера')
+break;
+    case 21:
+    alert('Девять часов вечера')
+break;
+    case 22:
+    alert('Десять часов вечера')
+break;
+    case 23:
+    alert('Одиннадцать часов вечера')
+break;
+    case 24:
+    alert('Двеннадцать часов полуночи')
+break;
+default:
+alert('Ошибка')
+break;
+}
+
+var a = +prompt('Введите первое число')
+var b = +prompt('Ведите второе число')
+var c = +prompt('Введите третье число')
+
+
+if(a >= b && a <= c||b >= a && b >=c && a >= c){
+    alert('Ваша цифра ' + a)
+}else if(b >= a && b <= c||a >= b && a >=c && b>=c){
+    alert('Ваша цифра ' + b)
+}else if(c >= a && c <= b || c <= a && c >= b && a >= b){
+    alert('Ваша цифра ' + c)
+}else{
+    alert('Ошибка')
+}
+
+// if (age == 1&&2&&3&&4&&5&&6&&7&&8&&9&&10&&11&&12&&13&&14&&15&&16&&17){
+// alert(' вы еще молоды, вам нужно учиться')
+// }else if (age == 18&&19&&20&&21&&22&&23&&24&&25&&26&&27&&28&&29&&30&&31&&32&&33&&34&&35&&36&&37&&38&&39&&40&&41&&42&&43&&44&&45&&46&&47&&48&&49&&50){
+// alert('Вам нужно работать!')    
+// }else if(age == 51&&52&&53&&54&&55&&56&&57&&58&&59){
+// alert('Вам скоро на пенсию')
+// }else if(age == 61&&62&&63&&64&&65&&66&&67&&68&&69&&70&&71&&72&&73&&74&&75&&76&&77&&78&&79&&80&&81&&82&&83&&84&&85&&86&&87&&88&&89&&90&&91&&92&&93&&94&&95&&96&&97&&98&&99&&100){
+// alert('Вы пенсионер')
+// }
+// else{
+//     alert('Ошибка')
+// }
+
+
+
+
